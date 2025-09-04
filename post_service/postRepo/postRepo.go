042b72f53f037fc4b5ce89f71d90905027f3ddc8
https://github.com/alimx07 <149194464+alimx07@users.noreply.github.com>
2025-09-04 04:52:19 +0300

    feat(post_service): add transaction operations to ensure outbox pattern for post, comment, and like actions

--- post_service/postRepo/postRepo.go
+++ post_service/postRepo/postRepo.go
@@ -1,14 +1,16 @@
 package postRepo
 
 import (
+	"context"
+
 	"github.com/alimx07/Distributed_Microservices_Backend/post_service/models"
 )
 
 type PostRepo interface {
-	CreatePost(post models.Post) error
-	CreateComment(comment models.Comment) error
-	CreateLike(like models.Like) error
-	DeletePost(post models.Post) error
-	DeleteComment(comment models.Comment) error
-	DeleteLike(like models.Like) error
+	CreatePost(ctx context.Context, post models.Post) error
+	CreateComment(ctx context.Context, comment models.Comment) error
+	CreateLike(ctx context.Context, like models.Like) error
+	DeletePost(ctx context.Context, post models.Post) error
+	DeleteComment(ctx context.Context, comment models.Comment) error
+	DeleteLike(ctx context.Context, like models.Like) error
 }
--- post_service/postRepo/postgresRepo.go
+++ post_service/postRepo/postgresRepo.go
@@ -1,11 +1,14 @@
 package postRepo
 
 import (
+	"context"
 	"database/sql"
-	"fmt"
 	"log"
+	"time"
 
 	"github.com/alimx07/Distributed_Microservices_Backend/post_service/models"
+	pb "github.com/alimx07/Distributed_Microservices_Backend/services_bindings_go"
+	"google.golang.org/protobuf/proto"
 )
 
 type PostgresRepo struct {
@@ -17,90 +20,167 @@ func NewPostgresRepo(db *sql.DB) *PostgresRepo {
 		db: db,
 	}
 }
-func (ps *PostgresRepo) CreatePost(post models.Post) error {
-	query := `INSERT INTO posts (user_id, content) 
-              VALUES ($1, $2)`
-	_, err := ps.db.Exec(query, post.User_id, post.Content)
+func (ps *PostgresRepo) CreatePost(ctx context.Context, post models.Post) error {
+	tx, err := ps.db.BeginTx(ctx, nil)
+	var postId string
+	tx.QueryRowContext(ctx,
+		`INSERT INTO posts (user_id, content) 
+		VALUES ($1, $2) RETURNING  id`,
+		post.User_id, post.Content).Scan(&postId)
 	if err != nil {
-		log.Printf("Error creating post: %v", err)
-		return fmt.Errorf("failed to create post: %w", err)
+		tx.Rollback()
+		log.Println("Error creating post: ", err.Error())
+		return err
 	}
+	evt := &pb.KPostCreated{
+		Id:        postId,
+		UserId:    post.User_id,
+		Content:   post.Content,
+		CreatedAt: time.Now().UnixMilli(),
+	}
+	payload, _ := proto.Marshal(evt)
 
+	_, err = tx.ExecContext(ctx,
+		`INSERT INTO outbox (topic ,kafka_key, payload) VALUES ($1 , $2, $3)`,
+		"posts.created", post.User_id, payload)
+	if err != nil {
+		tx.Rollback()
+		log.Println("Failed to insert Post in outbox DB: ", err.Error())
+		return err
+	}
 	return nil
 }
-func (ps *PostgresRepo) CreateComment(comment models.Comment) error {
-	query := `INSERT INTO comments (post_id, user_id, content) 
-              VALUES ($1, $2, $3)`
-
-	_, err := ps.db.Exec(query, comment.Post_id, comment.User_id, comment.Content)
+func (ps *PostgresRepo) CreateComment(ctx context.Context, comment models.Comment) error {
+	tx, err := ps.db.BeginTx(ctx, nil)
+	var commentID int64
+	tx.QueryRowContext(ctx,
+		`INSERT INTO comments (user_id, post_id ,content) 
+		VALUES ($1, $2 , $3) RETURNING  id`,
+		comment.User_id, comment.Post_id, comment.Content).Scan(&commentID)
 	if err != nil {
-		log.Printf("Error creating comment: %v", err)
-		return fmt.Errorf("failed to create comment: %w", err)
+		tx.Rollback()
+		log.Println("Error creating Coment: ", err.Error())
+		return err
 	}
+	evt := &pb.KCommentCreated{
+		Id:        commentID,
+		UserId:    comment.User_id,
+		PostId:    comment.Post_id,
+		Content:   comment.Content,
+		CreatedAt: time.Now().UnixMilli(),
+	}
+	payload, _ := proto.Marshal(evt)
 
+	_, err = tx.ExecContext(ctx,
+		`INSERT INTO outbox (topic ,kafka_key, payload) VALUES ($1 , $2, $3)`,
+		"comments.created", comment.User_id, payload)
+	if err != nil {
+		tx.Rollback()
+		log.Println("Failed to insert Commnet in outbox DB: ", err.Error())
+		return err
+	}
 	return nil
 }
-func (ps *PostgresRepo) CreateLike(like models.Like) error {
-	query := `INSERT INTO likes (post_id, user_id) 
-              VALUES ($1, $2)`
-
-	_, err := ps.db.Exec(query, like.Post_id, like.User_id)
+func (ps *PostgresRepo) CreateLike(ctx context.Context, like models.Like) error {
+	tx, err := ps.db.BeginTx(ctx, nil)
+	var likeID int64
+	tx.QueryRowContext(ctx,
+		`INSERT INTO likes (user_id, post_id) 
+		VALUES ($1, $2) RETURNING  id`,
+		like.User_id, like.Post_id).Scan(&likeID)
 	if err != nil {
-		log.Printf("Error creating like: %v", err)
-		return fmt.Errorf("failed to create like: %w", err)
+		tx.Rollback()
+		log.Println("Error creating Like: ", err.Error())
+		return err
 	}
+	evt := &pb.KLikeCreated{
+		UserId:    like.User_id,
+		PostId:    like.Post_id,
+		CreatedAt: time.Now().UnixMilli(),
+	}
+	payload, _ := proto.Marshal(evt)
 
+	_, err = tx.ExecContext(ctx,
+		`INSERT INTO outbox (topic ,kafka_key, payload) VALUES ($1 , $2 , $3)`,
+		"likes.created", like.User_id, payload)
+	if err != nil {
+		tx.Rollback()
+		log.Println("Failed to insert Commnet in outbox DB: ", err.Error())
+		return err
+	}
 	return nil
 }
 
-func (ps *PostgresRepo) DeletePost(post models.Post) error {
-	query := `DELETE FROM posts WHERE id = $1`
-
-	result, err := ps.db.Exec(query, post.Id)
+func (ps *PostgresRepo) DeletePost(ctx context.Context, post models.Post) error {
+	tx, err := ps.db.BeginTx(ctx, nil)
+	tx.ExecContext(ctx,
+		`DELETE FROM posts where id = $1`, post.Id)
 	if err != nil {
-		log.Printf("Error deleting post: %v", err)
-		return fmt.Errorf("failed to delete post: %w", err)
+		tx.Rollback()
+		log.Printf("Error Deleting post{%v} : %v\n", post.Id, err.Error())
+		return err
 	}
-
-	rowsAffected, _ := result.RowsAffected()
-
-	if rowsAffected == 0 {
-		return fmt.Errorf("post with ID %v not found", post.Id)
+	evt := &pb.KPostDeleted{
+		Id: post.Id,
 	}
+	payload, _ := proto.Marshal(evt)
 
+	_, err = tx.ExecContext(ctx,
+		`INSERT INTO outbox (topic ,kafka_key, payload) VALUES ($1 , $2, $3)`,
+		"posts.deleted", post.User_id, payload)
+	if err != nil {
+		tx.Rollback()
+		log.Println("Failed to insert Post Deletion in outbox DB: ", err.Error())
+		return err
+	}
 	return nil
 }
-func (ps *PostgresRepo) DeleteComment(comment models.Comment) error {
-	query := `DELETE FROM comments WHERE id = $1`
-
-	result, err := ps.db.Exec(query, comment.Id)
+func (ps *PostgresRepo) DeleteComment(ctx context.Context, comment models.Comment) error {
+	tx, err := ps.db.BeginTx(ctx, nil)
+	tx.ExecContext(ctx,
+		`DELETE FROM comments where id = $1`, comment.Id)
 	if err != nil {
-		log.Printf("Error deleting comment: %v", err)
-		return fmt.Errorf("failed to delete comment: %w", err)
+		tx.Rollback()
+		log.Printf("Error Deleting comment{%v} : %v\n", comment.Id, err.Error())
+		return err
 	}
-
-	rowsAffected, _ := result.RowsAffected()
-
-	if rowsAffected == 0 {
-		return fmt.Errorf("comment with ID %v not found", comment.Id)
+	evt := &pb.KCommentDeleted{
+		Id: comment.Id,
 	}
+	payload, _ := proto.Marshal(evt)
 
+	_, err = tx.ExecContext(ctx,
+		`INSERT INTO outbox (topic ,kafka_key, payload) VALUES ($1 , $2, $3)`,
+		"comments.deleted", comment.User_id, payload)
+	if err != nil {
+		tx.Rollback()
+		log.Println("Failed to insert Comment Deletion in outbox DB: ", err.Error())
+		return err
+	}
 	return nil
 }
-func (ps *PostgresRepo) DeleteLike(like models.Like) error {
-	query := `DELETE FROM likes WHERE post_id = $1 AND user_id= $2`
-
-	result, err := ps.db.Exec(query, like.Post_id, like.User_id)
+func (ps *PostgresRepo) DeleteLike(ctx context.Context, like models.Like) error {
+	tx, err := ps.db.BeginTx(ctx, nil)
+	tx.ExecContext(ctx,
+		`DELETE FROM likes where user_id = $1 AND post_id = $2`, like.User_id, like.Post_id)
 	if err != nil {
-		log.Printf("Error deleting like: %v", err)
-		return fmt.Errorf("failed to delete like: %w", err)
+		tx.Rollback()
+		log.Printf("Error Deleting of user{%v} for post{%v} : %v\n", like.User_id, like.Post_id, err.Error())
+		return err
 	}
-
-	rowsAffected, _ := result.RowsAffected()
-
-	if rowsAffected == 0 {
-		return fmt.Errorf("like of user {%v} for post {%v} not found", like.User_id, like.Post_id)
+	evt := &pb.KLikeDeleted{
+		UserId: like.User_id,
+		PostId: like.Post_id,
 	}
+	payload, _ := proto.Marshal(evt)
 
+	_, err = tx.ExecContext(ctx,
+		`INSERT INTO outbox (topic ,kafka_key, payload) VALUES ($1 , $2, $3)`,
+		"posts.deleted", like.User_id, payload)
+	if err != nil {
+		tx.Rollback()
+		log.Println("Failed to insert Like Deletion in outbox DB: ", err.Error())
+		return err
+	}
 	return nil
 }
