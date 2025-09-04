13503202375cf0f963dd9a98a5d6e39baa76fa9d
https://github.com/alimx07 <149194464+alimx07@users.noreply.github.com>
2025-09-04 04:35:45 +0300

    feat(post_service): add KafkaConfig struct

--- post_service/models/models.go
+++ post_service/models/models.go
@@ -1,7 +1,5 @@
 package models
 
-import "time"
-
 type Config struct {
 	DBHost     string
 	DBPort     string
@@ -12,23 +10,32 @@ type Config struct {
 	ServerPort string
 }
 type Post struct {
-	Id             int
+	Id             int32
 	User_id        string
 	Content        string
-	Likes_count    int
-	Comments_count int
-	Created_at     time.Duration
+	Likes_count    int32
+	Comments_count int32
 }
 
 type Comment struct {
-	Id         int64
-	User_id    int
-	Post_id    int
-	Content    string
-	Created_at time.Duration
+	Id      int64
+	User_id int32
+	Post_id int32
+	Content string
 }
 
 type Like struct {
-	Post_id int
-	User_id int
+	Post_id int32
+	User_id int32
+}
+
+// another configs will be added later
+type KafkaConfig struct {
+	Brokers         []string
+	InputTopic      string
+	BatchSize       string
+	ComperssionType string
+	Acks            string
+	MaxInFlight     string
+	Idempotence     string
 }
