2d45816e9298e03b67fa7bcb44784bb827735696
https://github.com/alimx07 <149194464+alimx07@users.noreply.github.com>
2025-09-09 04:22:23 +0300

    feat(post_service): add docker-compose file with postgres,kafka,debezium images

---
+++ post_service/utils.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/alimx07/Distributed_Microservices_Backend/post_service/models"
+	_ "github.com/lib/pq"
+)
+
+func LoadConfig() (models.Config, error) {
+	config := models.Config{
+		DBHost:     os.Getenv("DB_HOST"),
+		DBPort:     os.Getenv("DB_PORT"),
+		DBUser:     os.Getenv("DB_USER"),
+		DBPassword: os.Getenv("DB_PASSWORD"),
+		DBName:     os.Getenv("DB_NAME"),
+		ServerPort: os.Getenv("SERVER_PORT"),
+		ServerHost: os.Getenv("SERVER_HOST"),
+	}
+	return config, nil
+}
+
+func InitDB(config models.Config) (*sql.DB, error) {
+	DBpath := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		config.DBHost, config.DBPort, config.DBUser, config.DBPassword, config.DBName)
+
+	DB, err := sql.Open("postgres", DBpath)
+	if err != nil {
+		log.Println("Failed to Connect with Post_service DB", err.Error())
+		return nil, err
+	}
+	return DB, nil
+}
