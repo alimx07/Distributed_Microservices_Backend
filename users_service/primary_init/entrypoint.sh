da3fd27506c37cae2e3a1c200c54da134d77dbeb
https://github.com/alimx07 <149194464+alimx07@users.noreply.github.com>
2025-10-10 19:53:10 +0300

    feat(user_service): add docker configs for service

---
+++ users_service/primary_init/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+
+echo "Configuring primary for streaming replication..."
+
+cat >> "$PGDATA/postgresql.conf" <<'EOF'
+# replication settings
+listen_addresses = '*'
+wal_level = replica
+max_wal_senders = 10
+wal_keep_size = 128MB
+hot_standby = on
+EOF
+
+echo "host replication physical_rep 0.0.0.0/0 md5" >> "$PGDATA/pg_hba.conf"
+
+
+echo "Primary configured."
