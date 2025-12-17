c252fdf1d9ef55eb00092a9e88ea8c875d5760bd
https://github.com/alimx07 <149194464+alimx07@users.noreply.github.com>
2025-12-17 04:46:37 +0200

    fix(feed_service): fix load configs

--- feed_service/models/models.go
+++ feed_service/models/models.go
@@ -9,33 +9,34 @@ type KafkaConfig struct {
 }
 
 type RedisConfig struct {
-	Addr     string
-	Port     string
-	Password string
+	ClusterAddr []string
+	Password    string
 }
 
 type ServerConfig struct {
-	ServerHost    string
-	ServerPort    string
-	PostService   string
-	UserService   string
-	FollowService string
+	ServerHost     string
+	ServerPort     string
+	ServerHTTPPort string
+	PostService    string
+	UserService    string
+	FollowService  string
 }
+
 type FeedItem struct {
-	PostId     int64 `json:"post_id"`
-	UserId     int64 `json:"user_id"`
-	Created_at int64 `json:"created_at"`
+	PostId     string `json:"post_id"`
+	UserId     string `json:"user_id"`
+	Created_at int64  `json:"created_at"`
 }
 
 type Cursor struct {
-	UserId   int64
+	UserId   string
 	Cursor   string
 	PageSize int32
 }
 
 type Post struct {
 	UserName       string
-	UserID         int32
+	UserID         string
 	Content        string
 	Created_at     int64
 	Likes_count    int64
--- feed_service/utils.go
+++ feed_service/utils.go
@@ -7,7 +7,6 @@ import (
 	"strings"
 
 	"github.com/alimx07/Distributed_Microservices_Backend/feed_service/models"
-	"github.com/joho/godotenv"
 )
 
 func ValidateCursorData(c string, pageSize int32) (string, int32) {
@@ -27,23 +26,26 @@ func ValidateCursorData(c string, pageSize int32) (string, int32) {
 }
 
 func LoadConfig() (models.ServerConfig, error) {
-	if err := godotenv.Load(".env"); err != nil {
-		return models.ServerConfig{}, err
-	}
 	config := models.ServerConfig{
-		ServerPort:    os.Getenv("SERVER_PORT"),
-		ServerHost:    os.Getenv("SERVER_HOST"),
-		PostService:   os.Getenv("POST_SERVICE"),
-		UserService:   os.Getenv("USER_SERVICE"),
-		FollowService: os.Getenv("FOLLOW_SERVICE"),
+		ServerPort:     os.Getenv("SERVER_PORT"),
+		ServerHost:     os.Getenv("SERVER_HOST"),
+		ServerHTTPPort: os.Getenv("SERVER_HTTP_PORT"),
+		PostService:    os.Getenv("POST_SERVICE"),
+		UserService:    os.Getenv("USER_SERVICE"),
+		FollowService:  os.Getenv("FOLLOW_SERVICE"),
+	}
+	return config, nil
+}
+
+func LoadRedisConfig() (models.RedisConfig, error) {
+	config := models.RedisConfig{
+		ClusterAddr: strings.Split(os.Getenv("CLUSTER_ADDR"), ","),
+		Password:    os.Getenv("CACHE_PASSWORD"),
 	}
 	return config, nil
 }
 
 func LoadkakfaConfig() (models.KafkaConfig, error) {
-	if err := godotenv.Load(".env"); err != nil {
-		return models.KafkaConfig{}, err
-	}
 	config := models.KafkaConfig{
 		BootStrapServers: os.Getenv("BOOTSTRAP_SERVERS"),
 		GroupID:          os.Getenv("GROUP_ID"),
