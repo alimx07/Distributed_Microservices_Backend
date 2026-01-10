f44f1506d631bbb80cb1397fa3150f9235a91acb
https://github.com/alimx07 <149194464+alimx07@users.noreply.github.com>
2026-01-10 17:09:23 +0200

    feat(follow_service): add push to serviceRegistery on starting

---
+++ follow_service/src/main/java/com/mini_x/follow_service/config/EtcdConfig.java
@@ -0,0 +1,21 @@
+package com.mini_x.follow_service.config;
+
+import io.etcd.jetcd.Client;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EtcdConfig {
+
+    @Value("${etcd.endpoints}")
+    private String etcdEndpoints;
+
+    @Bean
+    public Client etcdClient() {
+        String[] endpoints = etcdEndpoints.split(",");
+        return Client.builder()
+                .endpoints(endpoints)
+                .build();
+    }
+}
